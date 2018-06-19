a1e8768151da485dfc021b26bfa9209f52142581
Fredrik Lundkvist <freddelunk@gmail.com>
2018-06-19 15:23:38 +0200

    Bättre idé än innan
    Flyttade databas-interaktionerna till flask-sqlalchemy och lade till det i requirements.txt

---
+++ models.py
@@ -0,0 +1,21 @@
+from app import db
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.Text)
+    headline = db.Column(db.Text)
+    text = db.Column(db.Text)
+    tags = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '<Nyhet skriven av {}>'.format(self.author)
+
+    def as_dictionary(self):
+        news_dict = {
+            "id" : self.id,
+            "author" : self.author,
+            "text" : self.text,
+            "tags": self.tags
+        }
+
+        return news_dict
